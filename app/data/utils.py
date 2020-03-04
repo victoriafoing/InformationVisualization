a6c8c0e11a17c629c0ff9154ccbe3c97fc270ac3
Thomas Saint-Gérand <devspaceship@gmail.com>
2020-03-04 10:20:07 +0100

    Updated activity filtering

--- app/data/utils.py
+++ app/data/utils.py
@@ -3,13 +3,19 @@ from typing import List
 
 import pandas as pd
 
-def most_active_sr(infile: str, n: int) -> List[str]:
-    body = pd.read_csv(infile, delimiter='\t', index_col=0)
 
-    active_c = Counter(body['SOURCE_SUBREDDIT'])
+def most_active_sr(body_file: str, title_file: str, n: int) -> List[str]:
+    body = pd.read_csv(body_file, delimiter='\t', index_col=0)
+    title = pd.read_csv(title_file, delimiter='\t', index_col=0)
+
+    active_c = Counter()
+    active_c.update(body['SOURCE_SUBREDDIT'])
     active_c.update(body['TARGET_SUBREDDIT'])
+    active_c.update(title['SOURCE_SUBREDDIT'])
+    active_c.update(title['TARGET_SUBREDDIT'])
 
     return list(map(lambda x: x[0], active_c.most_common(n)))
 
+
 if __name__ == '__main__':
-    most_active_sr('reddit-body-filtered.tsv', 250)
+    print(most_active_sr('reddit-body-filtered.tsv', 'reddit-title-filtered.tsv', 200))
