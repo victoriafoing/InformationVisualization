b7157ee507d9626890531818583fd6154c43ea50
Thomas Saint-Gérand <devspaceship@gmail.com>
2020-03-04 12:11:47 +0100

    Added embedding_to_np_matrix

--- app/data/utils.py
+++ app/data/utils.py
@@ -1,6 +1,7 @@
 from collections import Counter
 from typing import List
 
+import numpy as np
 import pandas as pd
 
 
@@ -17,5 +18,24 @@ def most_active_sr(body_file: str, title_file: str, n: int) -> List[str]:
     return list(map(lambda x: x[0], active_c.most_common(n)))
 
 
+def embedding_to_np_matrix(infile: str) -> np.array:
+    df = pd.read_csv(infile, index_col='SUBREDDIT_ID')
+
+    def converter(vector):
+        vector = vector.split(' ')
+        vector = list(map(float, vector))
+        return vector
+
+    df['EMBEDDING'] = df['EMBEDDING'].apply(converter)
+
+    array = []
+    for i, e in enumerate(df.values):
+        array.append(np.array(e[0]))
+
+    return np.array(array)
+
+
 if __name__ == '__main__':
-    print(most_active_sr('reddit-body-filtered.tsv', 'reddit-title-filtered.tsv', 200))
+    print(
+        most_active_sr('reddit-body-filtered.tsv', 'reddit-title-filtered.tsv',
+                       200))
