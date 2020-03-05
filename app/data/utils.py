86b077e5bbe153dbd4c2da820ffc0f1c34873e79
Thomas Saint-Gérand <devspaceship@gmail.com>
2020-03-05 11:21:08 +0100

    Made embedding_to_np_matrix more versatile

--- app/data/utils.py
+++ app/data/utils.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from typing import List
+from typing import List, Union
 
 import numpy as np
 import pandas as pd
@@ -18,8 +18,9 @@ def most_active_sr(body_file: str, title_file: str, n: int) -> List[str]:
     return list(map(lambda x: x[0], active_c.most_common(n)))
 
 
-def embedding_to_np_matrix(infile: str) -> np.array:
-    df = pd.read_csv(infile, index_col='SUBREDDIT_ID')
+def embedding_to_np_matrix(df: Union[str, pd.DataFrame]) -> np.array:
+    if type(df) == str:
+        df = pd.read_csv(df)
 
     def converter(vector):
         vector = vector.split(' ')
@@ -29,13 +30,16 @@ def embedding_to_np_matrix(infile: str) -> np.array:
     df['EMBEDDING'] = df['EMBEDDING'].apply(converter)
 
     array = []
+    names = []
     for i, e in enumerate(df.values):
-        array.append(np.array(e[0]))
+        array.append(np.array(e[-1]))
+        names.append(df.loc[i, 'SUBREDDIT_ID'])
 
-    return np.array(array)
+    return names, np.array(array)
 
 
 if __name__ == '__main__':
-    print(
-        most_active_sr('reddit-body-filtered.tsv', 'reddit-title-filtered.tsv',
-                       200))
+    # print(
+    #     most_active_sr('reddit-body-filtered.tsv', 'reddit-title-filtered.tsv',
+    #                    200))
+    print(embedding_to_np_matrix('reddit-embedding-filtered.csv'))
