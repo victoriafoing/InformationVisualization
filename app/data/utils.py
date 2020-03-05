fe0c7bec1af46723f9c791a93855ec738497f6ad
Thomas Saint-Gérand <devspaceship@gmail.com>
2020-03-05 12:24:01 +0100

    Added dimensionality reduction

--- app/data/utils.py
+++ app/data/utils.py
@@ -18,28 +18,7 @@ def most_active_sr(body_file: str, title_file: str, n: int) -> List[str]:
     return list(map(lambda x: x[0], active_c.most_common(n)))
 
 
-def embedding_to_np_matrix(df: Union[str, pd.DataFrame]) -> np.array:
-    if type(df) == str:
-        df = pd.read_csv(df)
-
-    def converter(vector):
-        vector = vector.split(' ')
-        vector = list(map(float, vector))
-        return vector
-
-    df['EMBEDDING'] = df['EMBEDDING'].apply(converter)
-
-    array = []
-    names = []
-    for i, e in enumerate(df.values):
-        array.append(np.array(e[-1]))
-        names.append(df.loc[i, 'SUBREDDIT_ID'])
-
-    return names, np.array(array)
-
-
 if __name__ == '__main__':
-    # print(
-    #     most_active_sr('reddit-body-filtered.tsv', 'reddit-title-filtered.tsv',
-    #                    200))
-    print(embedding_to_np_matrix('reddit-embedding-filtered.csv'))
+    print(
+        most_active_sr('reddit-body-filtered.tsv', 'reddit-title-filtered.tsv',
+                       200))
---
+++ app/main/dim_reduction.py
@@ -0,0 +1,54 @@
+from typing import Union
+
+import numpy as np
+import pandas as pd
+from sklearn.manifold import TSNE
+
+
+def embedding_to_np_matrix(df: Union[str, pd.DataFrame]) -> np.array:
+    if type(df) == str:
+        df = pd.read_csv(df)
+
+    def converter(vector):
+        vector = vector.split(' ')
+        vector = list(map(float, vector))
+        return vector
+
+    df['EMBEDDING'] = df['EMBEDDING'].apply(converter)
+
+    array = []
+    names = []
+    for i, e in enumerate(df.values):
+        array.append(np.array(e[-1]))
+        names.append(df.loc[i, 'SUBREDDIT_ID'])
+
+    return names, np.array(array)
+
+
+def dim_reduct(embedding_df: Union[str, pd.DataFrame],
+               dim=2,
+               perplexity=30,
+               timing=False) -> dict:
+    names, array = embedding_to_np_matrix(embedding_df)
+    reductor = TSNE(dim, perplexity=perplexity)
+
+    if timing:
+        import time
+        start = time.time()
+
+    array = reductor.fit_transform(array)
+
+    if timing:
+        print(f'Projection took {round(time.time() - start, 1)}s\n')
+
+    embeddings = {}
+    for name, proj in zip(names, array):
+        embeddings[name] = list(proj)
+
+    return embeddings
+
+
+if __name__ == '__main__':
+    embeddings = dim_reduct('../data/reddit-embedding-filtered.csv')
+    print(embeddings)
+    pass
