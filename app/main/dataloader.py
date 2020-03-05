34a4fa17baba7dde29a326595774ebbb6a17f435
Thomas Saint-Gérand <devspaceship@gmail.com>
2020-03-05 11:29:13 +0100

    yapfed dataloader

--- app/main/dataloader.py
+++ app/main/dataloader.py
@@ -23,7 +23,6 @@ def load_csv(file: str):
     return df
 
 
-
 def get_timeline_data(df: pd.DataFrame):
     result = df.groupby([df['TIMESTAMP'].dt.year, df['TIMESTAMP'].dt.month
                          ])['LINK_SENTIMENT'].sum().sort_values()
@@ -38,29 +37,31 @@ def load_fake_data():
     df = pd.DataFrame(data=d)
     return df
 
+
 def filter_year(year, df: pd.DataFrame):
     return df[df['TIMESTAMP'].dt.year == year]
+
 
 def filter_month(month, df: pd.DataFrame):
     return df[df['TIMESTAMP'].dt.month == month]
 
+
 def calc_stats(df):
     result = {
-        "mean" : df['LINK_SENTIMENT'].mean(),
-        "std" : df['LINK_SENTIMENT'].std()
+        "mean": df['LINK_SENTIMENT'].mean(),
+        "std": df['LINK_SENTIMENT'].std()
     }
     return result
 
+
 def calc_stats_for_month_year(df, month, year):
-    filtered_df = filter_year(int(year),filter_month(int(month),df))
+    filtered_df = filter_year(int(year), filter_month(int(month), df))
     if filtered_df.size > 0:
         return calc_stats(filtered_df)
     else:
-        return {
-        "mean" : 0.0,
-        "std" : 0.0
-    }
-    
+        return {"mean": 0.0, "std": 0.0}
+
+
 # def get_top_5(df: pd.Dataframe, month: int, sent: int, is_tar: bool):
 #     '''
 #     :param sent: 1 for positive, -1 for negative sentiment.
@@ -85,7 +86,8 @@ def calc_stats_for_month_year(df, month, year):
 #     df_src = pd.DataFrame(store.items(), columns=['Sr', 'Sent_count'])
 #     return df_src.nlargest(5, 'Sent_count')
 
-def get_top_5(df, month, year, sent: bool, is_tar: bool, n = 5):
+
+def get_top_5(df, month, year, sent: bool, is_tar: bool, n=5):
     '''
     :param sent: True for positive, False for negative sentiment
     :param is_tar: 
@@ -94,37 +96,43 @@ def get_top_5(df, month, year, sent: bool, is_tar: bool, n = 5):
         df = df[df['LINK_SENTIMENT'] > 0]
     else:
         df = df[df['LINK_SENTIMENT'] < 0]
-        df['LINK_SENTIMENT'] = df['LINK_SENTIMENT']*-1
+        df['LINK_SENTIMENT'] = df['LINK_SENTIMENT'] * -1
 
     filtered_df = filter_year(int(year), filter_month(int(month), df))
-    
+
     if is_tar:
         direction = 'TARGET_SUBREDDIT'
     else:
         direction = 'SOURCE_SUBREDDIT'
 
-    grouped = list(filtered_df.groupby(direction)['LINK_SENTIMENT'].sum().sort_values().items())
+    grouped = list(
+        filtered_df.groupby(direction)
+        ['LINK_SENTIMENT'].sum().sort_values().items())
     top = grouped[-n:]
     return {
-        "names": [ele[0] for ele in top], # lol,the_donald,etc
-        "count" : [ele[1] for ele in top] # 150, 112, etc
+        "names": [ele[0] for ele in top],  # lol,the_donald,etc
+        "count": [ele[1] for ele in top]  # 150, 112, etc
     }
+
 
 def get_most_hated_loved_subreddits_by_month_year(df, month, year):
     filtered_df = filter_year(int(year), filter_month(int(month), df))
-    grouped = list(filtered_df.groupby('TARGET_SUBREDDIT')['LINK_SENTIMENT'].sum().sort_values().items())
+    grouped = list(
+        filtered_df.groupby('TARGET_SUBREDDIT')
+        ['LINK_SENTIMENT'].sum().sort_values().items())
     most_hated = grouped[0]
     most_loved = grouped[-1]
     return {
-        "hated" : {
+        "hated": {
             "name": most_hated[0],
-            "count" : most_hated[1]
+            "count": most_hated[1]
         },
         "loved": {
             "name": most_loved[0],
             "count": most_loved[1]
         }
     }
+
 
 if __name__ == '__main__':
 
