88e7ec5da5c6deaa734a621a75248201ae4bdf52
victoriafoing <vickyfoing@gmail.com>
2020-03-08 15:34:54 +0100

    Moved code for barchart to JS file

--- app/static/js/barchart.js
+++ app/static/js/barchart.js
@@ -1,127 +1,151 @@
-function get_info_on_var(variable) {
-    var rel_meta = meta_data.find(function (d) {
-        return d.Variabele == variable;
-    })
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+// verify our websocket connection is established
+socket.on('connect', function () {
+    console.log('Websocket connected!');
+});
 
-    var label = rel_meta['Label_1'];
-    var definition = rel_meta['Definition'];
+var fetch_url = '/top/2017/02';
+fetch(fetch_url)
+     .then(function (response) {
+         return response.json();
+     })
+     .then((data) => {
+         var margin = {
+            top: 15,
+            right: 100,
+            bottom: 15,
+            left: 150
+        };
 
-    return [label, definition]
-}
-
-var width = 1000;
-var height = 700;
-var MOUSEOVER = false;
-
-var svgContainer = d3.select("#barchart").append("svg")
-    .attr("height", height)
-    .attr("width", width);
-
-var chart_height = 400,
-    chart_width = 700;
-
-// Code for vertical bar chart
-var x = d3.scaleBand().rangeRound([0, chart_width]).padding(0.1),
-    y = d3.scaleLinear().rangeRound([chart_height, 0]);
-
-// var x = d3.scaleLinear().rangeRound([0, chart_width]),
-//     y = d3.scaleBand().rangeRound([chart_height, 0]).padding(0.1);
-
-var chart_group = svgContainer.append("g")
-    .attr("id", "chart_group")
-    .attr("transform", "translate(" + 100 + "," + 50 + ")");
-
-// Code for vertical bar chart
-x.domain(x_variables);
-y.domain([0, 100]);
-
-console.log('x_variables : ', x_variables);
-
-// x.domain([0, 100]);
-// y.domain(x_variables);
-
-chart_group.append("g")
-    .attr("transform", "translate(" + 0 + "," + chart_height + ")")
-    .call(d3.axisBottom(x));
-// Code for vertical bar chart
-chart_group.selectAll("text")
-    .attr("y", 0)
-    .attr("x", 9)
-    .attr("transform", "rotate(90)")
-    .style("text-anchor", "start");
-
-chart_group.append("g")
-    .call(d3.axisLeft(y));
-
-var map = d3.map(data[0]);
-console.log('map.entries() : ', map.entries())
+         // Create width and height
+         var width =  2*960 - margin.left - margin.right + 200,
+             height = 500 - margin.top - margin.bottom;
+          //var width =  100,
+             //height = 100;
 
 
-// text label for the y axis
-svgContainer.append("text")
-    .attr("transform",
-        "translate(" + 35 + "," + (height / 2 - 100) + ")")
-    .style("text-anchor", "middle")
-    .style("font-size", "13px")
-    .text("Percentage");
+         var spread = 40;
+         // Create svg
+         var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-chart_group.append("text")
-    .attr("class", "title")
-    .attr("id", "chart-title")
-    .attr("y", -25)
-    .attr("x", chart_width / 2)
-    .style("font-weight", "bold")
-    .style("text-anchor", "middle")
-    .text("Rental statistics of " + selected_area);
+         // X will be a linear scale from 0 to max
+         var xLeft = d3.scaleLinear()
+            .range([0, width/2 - spread])
+            .domain([0, d3.max(data.hated, function (d) {
+                return d.count;
+            })]);
 
-// Code for vertical circle chart
-chart_group.selectAll(".circle")
-    .data(map.entries())
-    .enter()
-    .append("circle")
-    .attr("class", "circle")
-    .attr("cx", function (d) {
-        console.log(x(d.key));
-        return (x(d.key) + (x.bandwidth()/2));
-    })
-    .attr("cy", y(0))
-    .attr("r", 15)
-    .attr("fill", "#29e354")
-    .on("mouseover", function (d, i) {
-        var x_var = d.key;
-        var value = d.value;
-        var info = get_info_on_var(x_var);
-        var label = info[0]
-        var definition = info[1];
+          var xRight = d3.scaleLinear()
+            .range([0, width/2 - spread])
+            .domain([0, d3.max(data.loved, function (d) {
+                return d.count;
+            })]);
 
-        displayTooltip("<b>Variable: </b>" + label + "<br /><b>Percentage: </b>" +
-            value + "%<br /><b>Explanation: </b>" + definition)
+         // Y will be an ordinal scale with the names
+         var yLeft = d3.scaleBand()
+             .rangeRound([height, 0])
+             .padding(0.1)
+             .domain(data.hated.map(function (d) {
+                return d.name;
+             }));
 
-        //d3.select(this).attr("fill", "DarkOrange");
-    })
-    .on("mousemove", function (d, i) {
-        var x_var = d.key;
-        var value = d.value;
-        var info = get_info_on_var(x_var);
-        var label = info[0]
-        var definition = info[1];
+         var yRight = d3.scaleBand()
+             .rangeRound([height, 0])
+             .padding(0.1)
+             .domain(data.loved.map(function (d) {
+                return d.name;
+             }));
 
-        displayTooltip("<b>Variable: </b>" + label + "<br /><b>Percentage: </b>" +
-            value + "%<br /><b>Explanation: </b>" + definition)
+         var yAxisLeft = d3.axisLeft(yLeft)
+            .scale(yLeft)
+            .tickSize(0);
 
-        //d3.select(this).attr("fill", "DarkOrange");
-    })
-    .on("mouseout", function (d) {
-        hideTooltip();
-        //d3.select(this).attr("fill", "steelblue");
-    });
+         var yAxisRight = d3.axisRight(yRight)
+            .scale(yRight)
+            .tickSize(0);
 
-var circle_transition = d3.transition()
-    .duration(750)
-    .ease(d3.easeLinear);
+         var gy = svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxisLeft);
 
-chart_group.selectAll(".circle")
-    .transition(circle_transition)
-    .attr("cy", function (d) {
-        return y(d.value);
-    });
+         var gy = svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(" + width  +", 0)")
+            .call(yAxisRight);
+
+         var barsLeft = svg.selectAll(".bar")
+            .data(data.hated)
+            .enter()
+            .append("g");
+
+
+         var barsRight = svg.selectAll(".bar")
+            .data(data.loved)
+            .enter()
+            .append("g");
+
+         console.log('appending bars');
+
+         // Append bars
+         barsLeft.append("rect")
+            .attr("class", "bar")
+            .attr("y", function (d) {
+                return yLeft(d.name);
+            })
+            .attr("height", yLeft.bandwidth())
+            .attr("x", 0)
+            .attr("width", function (d) {
+                return xLeft(d.count);
+            });
+
+         //add a value label to the right of each bar
+        barsLeft.append("text")
+            .attr("class", "label")
+            //y position of the label is halfway down the bar
+            .attr("y", function (d) {
+                return yLeft(d.name) + yLeft.bandwidth() / 2 + 4;
+            })
+            //x position is 3 pixels to the right of the bar
+            .attr("x", function (d) {
+                return xLeft(d.count) + 3;
+            })
+            .text(function (d) {
+                return d.count;
+            });
+
+         barsRight.append("rect")
+            .attr("class", "bargood")
+            .attr("y", function (d) {
+                return yRight(d.name);
+            })
+            .attr("height", yRight.bandwidth())
+            .attr("x", function (d) {
+                return width - xRight(d.count);
+            })
+            .attr("width", function (d) {
+                return xRight(d.count);
+            });
+
+         //add a value label to the right of each bar
+        barsRight.append("text")
+            .attr("class", "label")
+            //y position of the label is halfway down the bar
+            .attr("y", function (d) {
+                return yRight(d.name) + yRight.bandwidth() / 2 + 4;
+            })
+            //x position is 3 pixels to the right of the bar
+            .attr("x", function (d) {
+                return width - xRight(d.count) - 40;
+            })
+            .text(function (d) {
+                return d.count;
+            });
+
+        console.log("end");
+        d3.select("#barchart").append("svg")
+
+});
