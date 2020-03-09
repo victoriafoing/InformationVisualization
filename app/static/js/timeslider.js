4b742d201660ea1ffa58201feb4580842984a691
wolf <fwolf@deloitte.nl>
2020-03-09 13:34:48 +0100

    Timeslider mockup

--- app/static/js/timeslider.js
+++ app/static/js/timeslider.js
@@ -8,7 +8,7 @@ var formatDateIntoYear = d3.timeFormat("%Y");
 var formatDate = d3.timeFormat("%b %Y");
 var parseDate = d3.timeParse("%m/%d/%y");
 
-var startDate = new Date("2004-11-01"),
+var startDate = new Date("2013-11-01"),
     endDate = new Date("2017-04-01");
 
 var margin = {top:150, right:50, bottom:0, left:50},
