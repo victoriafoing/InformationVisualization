29fc3a084b577cdb34449218cc30005744e58d6d
wolf <fwolf@deloitte.nl>
2020-03-13 19:44:43 +0100

    Draging fixed.

--- app/static/js/barchart.js
+++ app/static/js/barchart.js
@@ -7,9 +7,9 @@ socket.on('connect', function () {
 /// Here goes the timeslider
 var margin = {top:150, right:50, bottom:0, left:50},
     width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 100 - margin.top - margin.bottom;
 
-var svg = d3.select("#vis")
+var svg = d3.select("#timeslider")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom);  
@@ -21,7 +21,7 @@ var formatDate4bar = d3.timeFormat("/top/" + "%Y/ %m");
 var parseDate = d3.timeParse("%m/%y");
 
 var startDate = new Date("2015-01"),
-    endDate = new Date("2015-12");
+    endDate = new Date("2016-12");
 var moving = false;
 var currentValue = 0;
 var targetValue = width;
@@ -35,47 +35,10 @@ var x = d3.scaleTime()
 
 var slider = svg.append("g")
     .attr("class", "slider")
-    .attr("transform", "translate(" + margin.left + "," + height/5 + ")");
+    .attr("transform", "translate(" + margin.left + "," + (height+margin.top/2) + ")");
 
-slider.append("line")
-    .attr("class", "track")
-    .attr("x1", x.range()[0])
-    .attr("x2", x.range()[1])
-  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "track-inset")
-  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "track-overlay")
-    .call(d3.drag()
-        .on("start.interrupt", function() { slider.interrupt(); })
-        .on("start drag", function() {
-          currentValue = d3.event.x;
-          update(x.invert(currentValue)); 
-        })
-    );
-
-slider.insert("g", ".track-overlay")
-    .attr("class", "ticks")
-    .attr("transform", "translate(0," + 18 + ")")
-  .selectAll("text")
-    .data(x.ticks(12))
-    .enter()
-    .append("text")
-    .attr("x", x)
-    .attr("y", 10)
-    .attr("text-anchor", "middle")
-    .text(function(d) { return formatDate(d); });
-
-var handle = slider.insert("circle", ".track-overlay")
-    .attr("class", "handle")
-    .attr("r", 9);
-
-var label = slider.append("text")  
-    .attr("class", "label")
-    .attr("text-anchor", "middle")
-    .text(formatDate(startDate))
-    .attr("transform", "translate(0," + (-25) + ")")
-
- /// End of slider
+var barsLeft = svg.selectAll(".bar");
+var barsRight = svg.selectAll(".bargood");
 
 var fetch_url = '/top/2016/02';
 fetch(fetch_url) 
@@ -88,7 +51,48 @@ fetch(fetch_url)
             right: 100,
             bottom: 15,
             left: 150
-        }
+            }
+
+        slider.append("line")
+            .attr("class", "track")
+            .attr("x1", x.range()[0])
+            .attr("x2", x.range()[1])
+        .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+            .attr("class", "track-inset")
+        .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+            .attr("class", "track-overlay")
+            .call(d3.drag()
+                .on("start.interrupt", function() { slider.interrupt(); })
+                .on("end", function() {
+                currentValue = d3.event.x;
+                update(x.invert(currentValue)); 
+                })
+            );
+
+        slider.insert("g", ".track-overlay")
+            .attr("class", "ticks")
+            .attr("transform", "translate(0," + 18 + ")")
+        .selectAll("text")
+            .data(x.ticks(12))
+            .enter()
+            .append("text")
+            .attr("x", x)
+            .attr("y", 10)
+            .attr("text-anchor", "middle")
+            .text(function(d) { return formatDate(d); });
+
+        var handle = slider.insert("circle", ".track-overlay")
+            .attr("class", "handle")
+            .attr("r", 9);
+
+        var label = slider.append("text")  
+            .attr("class", "label")
+            .attr("text-anchor", "middle")
+            .text(formatDate(startDate))
+            .attr("transform", "translate(0," + (-25) + ")")
+
+        /// End of slider
+
 
         // Create width and height
         // var width = 2 * 960 - margin.left - margin.right + 200,
@@ -108,145 +112,211 @@ fetch(fetch_url)
 */
         // X will be a linear scale from 0 to max
 
-        var xLeft = d3.scaleLinear()
-            .range([0, width / 2 - spread])
-            .domain([0, d3.max(data.hated, function (d) {
-                return d.count;
-            })]);
 
-        var xRight = d3.scaleLinear()
-            .range([0, width / 2 - spread])
-            .domain([0, d3.max(data.loved, function (d) {
-                return d.count;
-            })]);
+        function makeBars(data) {
+            var xLeft = d3.scaleLinear()
+                .range([0, width / 2 - spread])
+                .domain([0, d3.max(data.hated, function (d) {
+                    return d.count;
+                })]);
 
-        // Y will be an ordinal scale with the names
-        var yLeft = d3.scaleBand()
-            .rangeRound([height, 0])
-            .padding(0.1)
-            .domain(data.hated.map(function (d) {
-                return d.name;
-            }));
+            var xRight = d3.scaleLinear()
+                .range([0, width / 2 - spread])
+                .domain([0, d3.max(data.loved, function (d) {
+                    return d.count;
+                })]);
 
-        var yRight = d3.scaleBand()
-            .rangeRound([height, 0])
-            .padding(0.1)
-            .domain(data.loved.map(function (d) {
-                return d.name;
-            }));
+            // Y will be an ordinal scale with the names
+            var yLeft = d3.scaleBand()
+                .rangeRound([height, 0])
+                .padding(0.1)
+                .domain(data.hated.map(function (d) {
+                    return d.name;
+                }));
 
-        var yAxisLeft = d3.axisLeft(yLeft)
-            .scale(yLeft)
-            .tickSize(0);
+            var yRight = d3.scaleBand()
+                .rangeRound([height, 0])
+                .padding(0.1)
+                .domain(data.loved.map(function (d) {
+                    return d.name;
+                }));
 
-        var yAxisRight = d3.axisRight(yRight)
-            .scale(yRight)
-            .tickSize(0);
+            var yAxisLeft = d3.axisLeft(yLeft)
+                .scale(yLeft)
+                .tickSize(0);
 
-        var gy = svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxisLeft);
+            var yAxisRight = d3.axisRight(yRight)
+                .scale(yRight)
+                .tickSize(0);
 
-        var gy = svg.append("g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(" + width + ", 0)")
-            .call(yAxisRight);
+            var gy = svg.append("g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(" + -width + ", 0)")
+                .call(yAxisLeft);
 
-        var barsLeft = svg.selectAll(".bar")
-            .data(data.hated)
-            .enter()
-            .append("g")
-            .on("click", function(d){
-                console.log(d.name);
-                var fetch_url = '/activity/'+d.name;
-                fetch(fetch_url)
-                    .then(function (response) {
-                        return response.json();
-                    })
-                    .then((data) => activity_timeline(data, d.name))
-            });
+            var gy = svg.append("g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(" + width + ", 0)")
+                .call(yAxisRight);
+                
+            barsLeft = svg.selectAll(".bar")
+                .data(data.hated)
+                .enter()
+                .append("g")
+                .on("click", function(d){
+                    var fetch_url = '/activity/'+d.name;
+                    fetch(fetch_url)
+                        .then(function (response) {
+                            return response.json();
+                        })
+                        .then((data) => {
+                            activity_timeline(data, d.name)
+                        })
+                });
 
 
-        var barsRight = svg.selectAll(".bar")
-            .data(data.loved)
-            .enter()
-            .append("g")
-            .on("click", function(d){
-                console.log(d.name);
-                var fetch_url = '/activity/'+d.name;
-                fetch(fetch_url)
-                    .then(function (response) {
-                        return response.json();
-                    })
-                    .then((data) => activity_timeline(data, d.name))
-            });
+            barsRight = svg.selectAll(".bargood")
+                .data(data.loved)
+                .enter()
+                .append("g")
+                .on("click", function(d){
+                    var fetch_url = '/activity/'+d.name;
+                    fetch(fetch_url)
+                        .then(function (response) {
+                            return response.json();
+                        })
+                        .then((data) => {
+                            activity_timeline(data, d.name)
+                        })
+                });
+            
+
+
+            console.log('appending bars');
+
+            // Append bars
+            barsLeft.append("rect")
+                .attr("class", "bar")
+                .attr("y", function (d) {
+                    return yLeft(d.name);
+                })
+                .attr("height", yLeft.bandwidth())
+                .attr("x", 0)
+                .attr("width", function (d) {
+                    return xLeft(d.count);
+                });
+
+            //add a value label to the right of each bar
+            barsLeft.append("text")
+                .attr("class", "label")
+                //y position of the label is halfway down the bar
+                .attr("y", function (d) {
+                    return yLeft(d.name) + yLeft.bandwidth() / 2 + 4;
+                })
+                //x position is 3 pixels to the right of the bar
+                .attr("x", function (d) {
+                    return xLeft(d.count) + 3;
+                })
+                .text(function (d) {
+                    return d.count;
+                });
+
+            barsRight.append("rect")
+                .attr("class", "bargood")
+                .attr("y", function (d) {
+                    return yRight(d.name);
+                })
+                .attr("height", yRight.bandwidth())
+                .attr("x", function (d) {
+                    return width - xRight(d.count);
+                })
+                .attr("width", function (d) {
+                    return xRight(d.count);
+                });
+
+            //add a value label to the right of each bar
+            barsRight.append("text")
+                .attr("class", "label")
+                //y position of the label is halfway down the bar
+                .attr("y", function (d) {
+                    return yRight(d.name) + yRight.bandwidth() / 2 + 4;
+                })
+                //x position is 3 pixels to the right of the bar
+                .attr("x", function (d) {
+                    return width - xRight(d.count) - 40;
+                })
+                .text(function (d) {
+                    return d.count;
+                });
+
+            svg.append("text")
+                .attr("class", "title")
+                .attr("x", (width / 2))
+                .attr("y", margin.top)
+                .attr("text-anchor", "middle")
+                .text("Interactive timeline");
+        }
+
+        makeBars(data);
+        console.log("end");
+/// Function to connect the slider to the bars
+
+      
+          playButton
+            .on("click", function() {
+            var button = d3.select(this);
+            if (button.text() == "Pause") {
+              moving = false;
+              clearInterval(timer);
+              // timer = 0;
+              button.text("Play");
+            } else {
+              moving = true;
+              timer = setInterval(step, 1500);
+              button.text("Pause");
+            }
+            console.log("Slider moving: " + moving);
+          })
         
 
+        
+  
+    function step() {
+        update(x.invert(currentValue));
+        currentValue = currentValue + (targetValue/24);
+        if (currentValue > targetValue) {
+          moving = false;
+          currentValue = 0;
+          clearInterval(timer);
+          // timer = 0;
+          playButton.text("Play");
+          console.log("Slider moving: " + moving);
+        }
+      }
+      
+      
+      function update(h) {
+        // update position and text of label according to slider scale
+        handle.attr("cx", x(h));
+        label
+          .attr("x", x(h))
+          .text(formatDate(h));
+      
+        // filter data set and redraw plot
+        fetch(formatDate4bar(h)) 
+              .then(function (response) {
+                  return response.json()
+              })
+              .then((newData) => {
+                updateBars(newData);
+              });
+      }
 
-        console.log('appending bars');
+      function updateBars(data) {
+          barsLeft.remove().exit();
+          barsRight.remove().exit();
 
-        // Append bars
-        barsLeft.append("rect")
-            .attr("class", "bar")
-            .attr("y", function (d) {
-                return yLeft(d.name);
-            })
-            .attr("height", yLeft.bandwidth())
-            .attr("x", 0)
-            .attr("width", function (d) {
-                return xLeft(d.count);
-            });
-
-        //add a value label to the right of each bar
-        barsLeft.append("text")
-            .attr("class", "label")
-            //y position of the label is halfway down the bar
-            .attr("y", function (d) {
-                return yLeft(d.name) + yLeft.bandwidth() / 2 + 4;
-            })
-            //x position is 3 pixels to the right of the bar
-            .attr("x", function (d) {
-                return xLeft(d.count) + 3;
-            })
-            .text(function (d) {
-                return d.count;
-            });
-
-        barsRight.append("rect")
-            .attr("class", "bargood")
-            .attr("y", function (d) {
-                return yRight(d.name);
-            })
-            .attr("height", yRight.bandwidth())
-            .attr("x", function (d) {
-                return width - xRight(d.count);
-            })
-            .attr("width", function (d) {
-                return xRight(d.count);
-            });
-
-        //add a value label to the right of each bar
-        barsRight.append("text")
-            .attr("class", "label")
-            //y position of the label is halfway down the bar
-            .attr("y", function (d) {
-                return yRight(d.name) + yRight.bandwidth() / 2 + 4;
-            })
-            //x position is 3 pixels to the right of the bar
-            .attr("x", function (d) {
-                return width - xRight(d.count) - 40;
-            })
-            .text(function (d) {
-                return d.count;
-            });
-
-        svg.append("text")
-            .attr("class", "title")
-            .attr("x", (width / 2))
-            .attr("y", margin.top)
-            .attr("text-anchor", "middle")
-            .text("Interactive timeline");
-
-
-        console.log("end");
-    });
+          makeBars(data);
+        }
+        
+    })
