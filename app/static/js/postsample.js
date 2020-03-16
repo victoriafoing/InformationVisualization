984a3d55e0e6fd719e05ce52880719b32b58a8bf
victoriafoing <vickyfoing@gmail.com>
2020-03-16 23:48:02 +0100

    Display date, source, target info above comment

--- app/static/js/postsample.js
+++ app/static/js/postsample.js
@@ -5,7 +5,10 @@ function sample_post(data) {
     var subreddit_url = "https://www.reddit.com/r/"+data.source
 
     // Create HTML
-    var myDiv = document.getElementById("comment");
-    var html = "<blockquote class=\"reddit-card\" data-card-created=\"1582734801\"><a id=\"post\" href="+post_url+"></a><a href="+subreddit_url+"></a></blockquote>"
-    myDiv.innerHTML = html;
+    var myDiv = document.getElementById("comment_box");
+    var html_stats = "<p style=\"text-align:center;padding-top:10px;\"><b>Date:</b> "+data.date+"&nbsp&nbsp&nbsp&nbsp<b>Source:</b> "+data.source+"&nbsp&nbsp&nbsp&nbsp<b>Target:</b> "+data.target+"</p>"
+    var div_open = "<div class=\"comment\" id=\"comment\">"
+    var html_block = "<blockquote class=\"reddit-card\" data-card-created=\"1582734801\"><a id=\"post\" href="+post_url+"></a><a href="+subreddit_url+"></a></blockquote>"
+    var div_close = "</div>"
+    myDiv.innerHTML = html_stats + div_open + html_block + div_close;
 }
