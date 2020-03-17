719a122d8501f753ba00a6875a92e086ad7d4c9d
wolf <fwolf@deloitte.nl>
2020-03-17 21:48:44 +0100

    typo

--- app/static/js/postsample.js
+++ app/static/js/postsample.js
@@ -6,7 +6,7 @@ function sample_post(data) {
 
     // Create HTML
     var myDiv = document.getElementById("comment_box");
-    var html_stats = "<p style=\"text-align:center;padding-top:10px;\"><b>Date:</b> "+data.date+"&nbsp&nbsp&nbsp&nbsp<b>Source:</b> "+data.source+"&nbsp&nbsp&nbsp&nbsp<b>Target:</b> "+data.target+"&nbsp&nbsp&nbsp&nbsp<b>Sentimnet:</b> "+data.sent+"</p>"
+    var html_stats = "<p style=\"text-align:center;padding-top:10px;\"><b>Date:</b> "+data.date+"&nbsp&nbsp&nbsp&nbsp<b>Source:</b> "+data.source+"&nbsp&nbsp&nbsp&nbsp<b>Target:</b> "+data.target+"&nbsp&nbsp&nbsp&nbsp<b>Sentiment:</b> "+data.sent+"</p>"
     var div_open = "<div class=\"comment\" id=\"comment\">"
     var html_block = "<blockquote class=\"reddit-card\" data-card-created=\"1582734801\"><a id=\"post\" href="+post_url+"></a><a href="+subreddit_url+"></a></blockquote>"
     var div_close = "</div>"
